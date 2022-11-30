3e31b56787afa49d4d0c3143cde2c01596e8df94
bryan-arriaza-g <bryan.arriaza.g@gmail.com>
2022-11-30 07:22:36 -0600

    feat(routing): header routing data
    * allows to navigate to specific back route page otherwise go to home route
    * added data props to handle and send data on history state

--- src/pages/tasks/merchandise-reception-task/components/TaskHeader/index.tsx
+++ src/pages/tasks/merchandise-reception-task/components/TaskHeader/index.tsx
@@ -7,19 +7,28 @@ import "./index.sass";
 
 interface ITaskHeaderProps {
   title: string;
+  backRoute?: string;
+  data?: unknown;
 }
 
-const TaskHeader: React.FC<ITaskHeaderProps> = ({ title }) => {
+const TaskHeader: React.FC<ITaskHeaderProps> = ({
+  title,
+  backRoute = '/',
+  data = {},
+}) => {
   const history = useHistory();
 
   return (
     <IonHeader className="task-header" mode="md">
       <IonToolbar mode="md">
-        <IonButton fill="clear" onClick={() => history.replace(`/`)}>
+        <IonButton
+          fill="clear"
+          onClick={() => history.replace(backRoute, data)}
+        >
           <IonIcon icon={arrowBack} slot="icon-only" size="large"></IonIcon>
         </IonButton>
         <XText className="ion-padding">
-          <h1 style={{ margin: 0, whiteSpace: "pre-line" }}>{title}</h1>
+          <h1 style={{ margin: 0, whiteSpace: 'pre-line' }}>{title}</h1>
         </XText>
       </IonToolbar>
     </IonHeader>
