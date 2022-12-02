0435278cade0eac4bf60d05019f99e06b41d87ae
bryan-arriaza-g <bryan.arriaza.g@gmail.com>
2022-12-01 21:28:08 -0600

    feat(api): added product client
    * added MerchandiseReceptionProducts api client
    * added Modes to load, initial and loaded data
    * added load() to get all product list data through api client
    * fake callApi to simulate api delay
    * rendered methods renderLOADING, renderPRODUCT_LOADED
    * renders to evaluate current step

--- src/pages/tasks/merchandise-reception-task/components/product/product-list/index.tsx
+++ src/pages/tasks/merchandise-reception-task/components/product/product-list/index.tsx
@@ -1,14 +1,22 @@
 import { useState, useEffect } from 'react';
-import { IonPage, IonContent, IonList } from '@ionic/react';
+import { IonPage, IonContent, IonList, IonLoading } from '@ionic/react';
 import { useHistory } from 'react-router';
 import { IProduct as IProductCategory } from '../../../../../../models/ITask';
 import { IProduct } from '../../../../../../models/IProduct';
 import TaskHeader from '../../TaskHeader';
 import Product from '../product';
+import { ProductSkeleton } from '../../../../../../components/loaders';
+import MerchandiseReceptionProducts from '../../../../../../clients/MerchandiseReceptionProducts';
 import './index.sass';
 
 import { routes } from '../../../../constants';
-import { mockProducts } from '../mockd';
+import { mockProducts } from '../mockd'; // Remove this one, is just to mock product lists by quantity
+
+enum Modes {
+  LOADING = 'LOADING',
+  INITIAL_STATE = 'INITIAL_STATE',
+  PRODUCT_LOADED = 'PRODUCT_LOADED',
+}
 
 interface ILocationState extends History {
   productCategory: IProductCategory;
@@ -18,16 +26,56 @@ interface ILocationState extends History {
 // Merchandise reception products list
 const Products: React.FC = () => {
   const history = useHistory<History>();
+  const [mode, setMode] = useState<Modes>(Modes.INITIAL_STATE);
+  const [products, setProducts] = useState<IProduct[]>();
   const locationState = history.location.state as ILocationState;
-  const [products, setProducts] = useState<IProduct[]>(
-    mockProducts(locationState?.productCategory?.total) // Replace by API
-  );
   const productCategories: IProductCategory[] = locationState.products;
   const productCategory: IProductCategory = locationState.productCategory || {};
 
   useEffect(() => {
     if (!history.location.state) history.replace('/');
   }, [history]);
+
+  const load = async () => {
+    setMode(Modes.LOADING);
+    const products =
+      await MerchandiseReceptionProducts.getMerchandiseReceptionProducts();
+    setProducts(mockProducts(products, productCategory.total));
+    setMode(Modes.PRODUCT_LOADED);
+  };
+
+  // Remove this function is just for testing to simulate api delay
+  const callApi = () => {
+    setTimeout(async () => {
+      await load();
+    }, 1000);
+  };
+
+  // TODO: Need's to replace by React Query ???
+  useEffect(() => {
+    callApi(); // Replace using load() directly instead to use callApi()
+  }, []);
+
+  const renderLOADING = () => <IonLoading isOpen={true}></IonLoading>;
+
+  const renderINITIAL_STATE = () => (
+    <ProductSkeleton qtyProducts={productCategory.total} />
+  );
+
+  const renderPRODUCT_LOADED = () => (
+    <IonList>
+      {products?.map((product: IProduct) => (
+        // Please check what should be the unique key ?
+        <Product key={product.block_type} product={product} />
+      ))}
+    </IonList>
+  );
+
+  const renders: Record<Modes, Function> = {
+    [Modes.LOADING]: renderLOADING,
+    [Modes.INITIAL_STATE]: renderINITIAL_STATE,
+    [Modes.PRODUCT_LOADED]: renderPRODUCT_LOADED,
+  };
 
   return (
     <IonPage>
@@ -37,11 +85,12 @@ const Products: React.FC = () => {
         data={{ products: productCategories }}
       />
       <IonContent className="ion-padding">
-        <IonList>
-          {products?.map((product: IProduct) => (
-            <Product key={product.shop} product={product} />
-          ))}
-        </IonList>
+        {(() => {
+          const renderModeFunction = renders[mode];
+          return (
+            (renderModeFunction && renderModeFunction()) || <div>{mode}</div>
+          );
+        })()}
       </IonContent>
     </IonPage>
   );
