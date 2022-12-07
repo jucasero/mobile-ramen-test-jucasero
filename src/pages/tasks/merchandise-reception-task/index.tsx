c76bf6249b2387416e17e80b00c3a94efd08d240
bryan-arriaza-g <bryan.arriaza.g@gmail.com>
2022-12-06 21:51:55 -0600

    chore(renamed): renamed component

--- src/pages/tasks/merchandise-reception-task/index.tsx
+++ src/pages/tasks/merchandise-reception-task/index.tsx
@@ -5,7 +5,7 @@ import TaskBody from "./components/task-body";
 
 import locales from "./locales";
 
-const MerchandisereceptionAlert: React.FC = () => {
+const MerchandiseReceptionAlert: React.FC = () => {
   const localize = i18(locales);
 
   return (
@@ -16,4 +16,4 @@ const MerchandisereceptionAlert: React.FC = () => {
   );
 };
 
-export default MerchandisereceptionAlert;
+export default MerchandiseReceptionAlert;
