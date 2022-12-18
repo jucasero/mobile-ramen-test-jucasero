604e34740960020cc34822c93f903447972f9925
bryan-arriaza-g <bryan.arriaza.g@gmail.com>
2022-12-18 01:56:02 -0600

    fix(style): header shadow bar

--- src/pages/tasks/merchandise-reception-task/components/task-header/index.tsx
+++ src/pages/tasks/merchandise-reception-task/components/task-header/index.tsx
@@ -19,22 +19,19 @@ const TaskHeader: React.FC<ITaskHeaderProps> = ({
   const history = useHistory();
 
   return (
-    <>
-      <IonHeader className="ion-no-border" mode="md">
-        <IonToolbar mode="md">
-          <IonButton
-            fill="clear"
-            onClick={() => history.replace(backRoute, data)}
-          >
-            <IonIcon icon={arrowBack} slot="icon-only" size="large" />
-          </IonButton>
-          <XText className="ion-padding">
-            <h1 style={{ margin: 0, whiteSpace: 'pre-line' }}>{title}</h1>
-          </XText>
-        </IonToolbar>
-      </IonHeader>
-      <div className="header-divider" />
-    </>
+    <IonHeader className="ion-no-border task-header" mode="md">
+      <IonToolbar mode="md">
+        <IonButton
+          fill="clear"
+          onClick={() => history.replace(backRoute, data)}
+        >
+          <IonIcon icon={arrowBack} slot="icon-only" size="large" />
+        </IonButton>
+        <XText className="ion-padding">
+          <h1 style={{ margin: 0, whiteSpace: 'pre-line' }}>{title}</h1>
+        </XText>
+      </IonToolbar>
+    </IonHeader>
   );
 };
 
