529adafa52b357f7977b7dcc475f7db4121e51c5
bryan-arriaza-g <bryan.arriaza.g@gmail.com>
2022-12-27 14:09:51 -0600

    feat(component): added sub category list
    * added component
    * added page view

---
+++ src/pages/tasks/found-rate/components/sub-categories/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+import { IonContent } from '@ionic/react';
+import { useHistory } from 'react-router';
+import { i18 } from '@team_eureka/eureka-ionic-core';
+import {
+  ICategory,
+  ISubCategory,
+} from '../../../../../models/found-rate/ICategory';
+import { ITask } from '../../../../../models/ITasks/ITask';
+import TaskHeader from '../../components/TaskHeader';
+import AccordionOption from '../../../../../components/accordion-option';
+import { routes } from '../../../../../routes'; // TODO: Needs to be separated by found-rate directory ?
+import locales from '../../locales';
+
+interface ILocationState {
+  category: ICategory;
+  task: ITask;
+}
+
+const SubCategories: React.FC = () => {
+  const history = useHistory<ILocationState>();
+  const locationState: ILocationState = history.location.state;
+  const categoryState: ICategory = locationState.category || {};
+  const localize = i18(locales);
+
+  useEffect(() => {
+    if (!categoryState) history.replace('/');
+  }, []);
+
+  return (
+    <>
+      <TaskHeader
+        title={categoryState.title}
+        subTitle={localize('FOUND_RATE_ALERT_TITLE', '')}
+        backRoute={routes.foundRate.root}
+        data={locationState.task}
+      />
+      <IonContent className='ion-padding'>
+        {categoryState.subCategories?.map((subCategory: ISubCategory) => (
+          <AccordionOption
+            key={subCategory.id}
+            title={subCategory.title}
+            total={subCategory.total}
+          >
+            <h3>Content</h3>
+          </AccordionOption>
+        ))}
+      </IonContent>
+    </>
+  );
+};
+
+export default SubCategories;
---
+++ src/pages/tasks/found-rate/views/subcategory-list/index.tsx
@@ -0,0 +1,12 @@
+import { IonPage } from '@ionic/react';
+import SubCategories from '../../components/sub-categories';
+
+const SubCategoryList: React.FC = () => {
+  return (
+    <IonPage>
+      <SubCategories />
+    </IonPage>
+  );
+};
+
+export default SubCategoryList;
