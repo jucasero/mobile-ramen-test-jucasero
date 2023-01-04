a2e38a078a86e0356e973732a598ac90feb3e1de
bryan-arriaza-g <bryan.arriaza.g@gmail.com>
2023-01-04 14:25:44 -0600

    feat(comp): added product component grid

--- src/pages/tasks/found-rate/components/product/index.tsx
+++ src/pages/tasks/found-rate/components/product/index.tsx
@@ -7,6 +7,7 @@ import {
   IonThumbnail,
   IonIcon,
   IonImg,
+  IonGrid,
 } from '@ionic/react';
 import { IProduct } from '../../../../../models/found-rate/IProduct';
 import shop from '../../../../../assets/media/shop.svg';
@@ -24,33 +25,35 @@ const Product: React.FC<IProps> = ({ product }) => {
   return (
     <div className='product-container'>
       <IonItem className='product-card-item' lines='none'>
-        <IonRow>
-          <IonCol size='4'>
-            <IonThumbnail>
-              <IonImg src={product.image} alt={product.description} />
-            </IonThumbnail>
-          </IonCol>
-          <IonCol>
-            <IonRow>
-              <div className='product-chip product-chip-icon'>
-                <IonIcon icon={shop} />
-              </div>
-              <div className='product-chip product-chip-nrt'>
-                <span>{localize('STOCK_NRT', '')}:</span>
-                <span className='product-chip-nrt-value'>
-                  {product.units_found}
-                </span>
-              </div>
-            </IonRow>
-            <IonRow>
-              <span className='product-title'>{product.description}</span>
-            </IonRow>
-            <IonRow className='product-ean'>
-              <span>{localize('EAN', '')}</span>
-              <span className='product-ean-code'>{product.ean}</span>
-            </IonRow>
-          </IonCol>
-        </IonRow>
+        <IonGrid>
+          <IonRow>
+            <IonCol size='3'>
+              <IonThumbnail>
+                <IonImg src={product.image} alt={product.description} />
+              </IonThumbnail>
+            </IonCol>
+            <IonCol className='product-detail'>
+              <IonRow>
+                <div className='product-chip product-chip-icon'>
+                  <IonIcon icon={shop} />
+                </div>
+                <div className='product-chip product-chip-nrt'>
+                  <span>{localize('STOCK_NRT', '')}:</span>
+                  <span className='product-chip-nrt-value'>
+                    {product.units_found}
+                  </span>
+                </div>
+              </IonRow>
+              <IonRow>
+                <span className='product-title'>{product.description}</span>
+              </IonRow>
+              <IonRow className='product-ean'>
+                <span>{localize('EAN', '')}</span>
+                <span className='product-ean-code'>{product.ean}</span>
+              </IonRow>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonItem>
     </div>
   );
