9b0e539af9584a5a06f82e45c354b5660bb7eb9e
bryan-arriaza-g <bryan.arriaza.g@gmail.com>
2023-01-04 11:18:27 -0600

    feat(component): added product component

---
+++ src/pages/tasks/found-rate/components/product/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { i18 } from '@team_eureka/eureka-ionic-core';
+import {
+  IonItem,
+  IonCol,
+  IonRow,
+  IonThumbnail,
+  IonIcon,
+  IonImg,
+} from '@ionic/react';
+import { IProduct } from '../../../../../models/found-rate/IProduct';
+import shop from '../../../../../assets/media/shop.svg';
+import locales from './locales';
+import './index.sass';
+
+const localize = i18(locales);
+
+interface IProps {
+  product: IProduct;
+}
+
+// Product card detail
+const Product: React.FC<IProps> = ({ product }) => {
+  return (
+    <div className='product-container'>
+      <IonItem className='product-card-item' lines='none'>
+        <IonRow>
+          <IonCol size='4'>
+            <IonThumbnail>
+              <IonImg src={product.image} alt={product.description} />
+            </IonThumbnail>
+          </IonCol>
+          <IonCol>
+            <IonRow>
+              <div className='product-chip product-chip-icon'>
+                <IonIcon icon={shop} />
+              </div>
+              <div className='product-chip product-chip-nrt'>
+                <span>{localize('STOCK_NRT', '')}:</span>
+                <span className='product-chip-nrt-value'>
+                  {product.units_found}
+                </span>
+              </div>
+            </IonRow>
+            <IonRow>
+              <span className='product-title'>{product.description}</span>
+            </IonRow>
+            <IonRow className='product-ean'>
+              <span>{localize('EAN', '')}</span>
+              <span className='product-ean-code'>{product.ean}</span>
+            </IonRow>
+          </IonCol>
+        </IonRow>
+      </IonItem>
+    </div>
+  );
+};
+
+export default Product;
